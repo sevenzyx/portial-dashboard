152f782982b882838ba6a767383c45ac05afb837
zhouyixin <eason.zhou@gmail.com>
2018-07-08 17:34:53 +0800

    change settings to sqlite3

--- portia_server/portia_server/settings.py
+++ portia_server/portia_server/settings.py
@@ -67,23 +67,23 @@ WSGI_APPLICATION = 'portia_server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'scrapy',
-        'USER': 'scrapy',
-        'PASSWORD': 'scrapy',
-        'HOST': '10.0.1.3',   # Or an IP Address that your DB is hosted on
-        'PORT': '3306',
-    }
-}
-
 #DATABASES = {
 #    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': 'scrapy',
+#        'USER': 'scrapy',
+#        'PASSWORD': 'scrapy',
+#        'HOST': '10.0.1.3',   # Or an IP Address that your DB is hosted on
+#        'PORT': '3306',
 #    }
 #}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
